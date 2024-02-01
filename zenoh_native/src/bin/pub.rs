5198fe9df136694d71391b60b9c59fe1f8f2b1bb
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2024-02-01 21:41:56 +0900

    enrich message and println

--- zenoh_native/src/bin/pub.rs
+++ zenoh_native/src/bin/pub.rs
@@ -8,11 +8,17 @@ async fn main() {
     let session = zenoh::open(config::peer()).res().await.unwrap().into_arc();
     let publisher = session.declare_publisher("key/expression").res().await.unwrap();
 
+    let msg_header = String::from("Hello from Rust!! ");
+    let mut index = 0;
     let handler = CtrlCHandler::new();
     while handler.should_continue() {
-        publisher.put("value").res().await.unwrap();
+        let msg: String = format!("{}{}", msg_header, index.to_string());
+        println!("Publishing: {}", msg);
+
+        publisher.put(msg).res().await.unwrap();
 
         thread::sleep(time::Duration::from_millis(1000));
+        index += 1;
     }
 
     publisher.delete().res().await.unwrap();
--- zenoh_native/src/bin/sub.rs
+++ zenoh_native/src/bin/sub.rs
@@ -4,7 +4,7 @@ use zenoh::prelude::r#async::*;
 async fn main() {
     let session = zenoh::open(config::default()).res().await.unwrap();
     let subscriber = session.declare_subscriber("key/expression").res().await.unwrap();
-    while let Ok(sample) = subscriber.recv_async().await {
-        println!("Received: {}", sample);
+    while let Ok(msg) = subscriber.recv_async().await {
+        println!("Subscribed: {}", msg.value);
     };
 }
