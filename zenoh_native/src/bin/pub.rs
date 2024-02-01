4f02911d9426bb23cefacb65bf3fe280188b0ede
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2024-02-01 21:10:39 +0900

    put forever until Ctrl+C

--- zenoh_native/src/bin/pub.rs
+++ zenoh_native/src/bin/pub.rs
@@ -1,8 +1,17 @@
 use zenoh::prelude::r#async::*;
 
+use ctrlc_handler::CtrlCHandler;
+use std::{thread, time};
+
 #[async_std::main]
 async fn main() {
+    let handler = CtrlCHandler::new();
+
     let session = zenoh::open(config::default()).res().await.unwrap();
-    session.put("key/expression", "value").res().await.unwrap();
+    while handler.should_continue() {
+        session.put("key/expression", "value").res().await.unwrap();
+
+        thread::sleep(time::Duration::from_millis(1000));
+    }
     session.close().res().await.unwrap();
 }
