ab556dbcbb5faf6442d1616b7d818461d04a619c
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2024-02-02 11:33:51 +0900

    add zenohex to mix dependency

--- zenoh_elixir/mix.exs
+++ zenoh_elixir/mix.exs
@@ -23,6 +23,7 @@ defmodule ZenohElixir.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:zenohex, "~> 0.1.5"}
     ]
   end
 end
