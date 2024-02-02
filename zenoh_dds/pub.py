df2f8e0e67f20a6e873de1848e7b2d68d2891739
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2024-02-02 16:57:12 +0900

    implement CycloneDDS pub node

--- zenoh_dds/pub.py
+++ zenoh_dds/pub.py
@@ -2,36 +2,25 @@ from dataclasses import dataclass
 from cyclonedds.domain import DomainParticipant
 from cyclonedds.core import Qos, Policy
 from cyclonedds.pub import DataWriter
-from cyclonedds.sub import DataReader
 from cyclonedds.topic import Topic
 from cyclonedds.idl import IdlStruct
-from cyclonedds.idl.annotations import key
 from time import sleep
-try:
-    from names import get_full_name
-    name = get_full_name()
-except:
-    import os
-    name = f"{os.getpid()}"
 
 # C, C++ require using IDL, Python doesn't
 @dataclass
 class Chatter(IdlStruct, typename="Chatter"):
-    name: str
-    key("name")
     message: str
-    count: int
 
 dp = DomainParticipant()
-tp = Topic(dp, "Hello", Chatter, qos=Qos(Policy.Reliability.Reliable(0)))
+tp = Topic(dp, "key/expression", Chatter)
 dw = DataWriter(dp, tp)
-dr = DataReader(dp, tp)
+
 count = 0
 while True:
-    sample = Chatter(name=name, message="Hello, World!", count=count)
-    count = count + 1
-    print("Writing ", sample)
-    dw.write(sample)
-    for sample in dr.take(10):
-        print("Read ", sample)
+    message = "Hello from DDS!! " + f"{count}"
+    msg = Chatter(message=message)
+    print("[pubdds]", msg.message)
+    dw.write(msg)
+    
     sleep(1)
+    count = count + 1
