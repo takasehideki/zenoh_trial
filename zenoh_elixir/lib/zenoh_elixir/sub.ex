4afc973429806f25b319cdda3d22bd4c6b8e5cd7
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2024-02-22 19:54:39 +0900

    fix code/API according to zenohex v0.2.0

--- zenoh_elixir/lib/zenoh_elixir/sub.ex
+++ zenoh_elixir/lib/zenoh_elixir/sub.ex
@@ -1,10 +1,17 @@
 defmodule ZenohElixir.Sub do
   def main do
-    session = Zenohex.open()
-    Session.declare_subscriber(session, "key/expression", fn msg -> sub_callback(msg) end)
+    {:ok, session} = Zenohex.open()
+    {:ok, subscriber} = Zenohex.Session.declare_subscriber(session, "key/expression")
+
+    spawn(ZenohElixir.Sub, :subscribe, [subscriber])
   end
 
-  def sub_callback(msg) do
-    IO.puts "[sub.ex] " <> msg
+  def subscribe(subscriber) do
+    case Zenohex.Subscriber.recv_timeout(subscriber) do
+      {:error, :timeout} -> nil
+      {:ok, msg} -> IO.puts "[sub.ex] " <> msg.value
+    end
+
+    subscribe(subscriber)
   end
 end
